50062577305bc3bbdc8aa8df7e8090fdc3dfe2aa
Will McKinnon <contact@willmckinnon.com>
2022-05-02 23:34:12 -0400

    ani-cli: 865fec2 -> 2.1

--- pkgs/ani-cli/default.nix
+++ pkgs/ani-cli/default.nix
@@ -1,24 +1,18 @@
 { stdenv, lib, fetchFromGitHub, makeWrapper, bash, mpv, curl, ffmpeg }:
 
-# https://nixos.wiki/wiki/Shell_Scripts
-
 stdenv.mkDerivation rec {
-  name = "ani-cli";
-  version ="1.0";
+  pname = "ani-cli";
+  version = "v2.1";
 
   src = fetchFromGitHub {
     owner="pystardust";
     repo="ani-cli";
-    rev="865fec2c72449169bd8b101665c70de77859f83c";
-    sha256="u3DosXWpq/uIiQjmugWLir701TDqfDnK7CrtfbT+jEM=";
+    rev=version;
+    sha256="A1c7YdBh2VOhw/xTvhNV50j9n+SELyRTHI5w+AeiWDs=";
   };
 
   buildInputs = [ bash mpv curl ffmpeg ];
   nativeBuildInputs = [ makeWrapper ];
-
-  configurePhase = ''
-    rm Makefile
-  '';
 
   installPhase = ''
     mkdir -p $out/bin
@@ -26,4 +20,11 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/ani-cli \
       --prefix PATH : ${lib.makeBinPath [ bash mpv curl ffmpeg ]}
   '';
+
+  meta = with lib; {
+    homepage = "https://github.com/pystardust/ani-cli";
+    description = "A cli tool to browse and play anime";
+    license = licenses.gpl3;
+    platforms = with platforms; linux ++ darwin;
+  };
 }
